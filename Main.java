9529aa75b1fdcd6f0b36748eab9d7fdaddabc0d9
toandang <toandang2000ab@gmail.com>
2022-04-09 00:15:54 +0700

    two commit

--- Main.java
+++ Main.java
@@ -1,7 +1,33 @@
 public class Main {
   public static void main(String[] args){
-    char letter = 'k';
-  
-    System.out.println(letter);
+    float myFloat = 35e3f;
+    double myDouble = 32E4d; 
+   
+    boolean isChecked = true;
+    String hello = "hello world";
+    char character = 'k';
+    char character2 =65; //ASCII
+     
+    System.out.println(character2 );
+    
   }
 }
+//*Kieu du lieu nguyen thuy(Primitive data types)
+//  -byte,short,int,long,float,double,boolean,char
+//convert double to int 
+//double b = 9.18d
+//int a = (int)b
+//*Kieu du lieu khong nguyen thuy(Primitive data types)
+//   -Kieu tham chieu
+//  - String,arrays,classes,interface
+//*Different
+//1Kieu nguyen thuy duoc xac dinh truoc(dinh ngia san trong java)
+//Kieu khong nguyen thuy duoc lap trinh vien tao va khong duoc dinh nghia san(tru String)
+//2.kieu khong nguyen thuy goi phuong thuc de thuc hien cac hoat dong nhat dinh 
+// kieu nguyen thuy khong the
+//3.Kieu nguyen thuy luon co gia tri
+//kieu khong nguyen thuy co the co gia tri null
+//4.Kieu nguyen thuy bat dau bang chu thuong 
+//kieu khong nguyen thuy bat dau bang chu in hoa 
+//5.Kich thuoc cua kieu nguyen thuy phu thuoc vao kieu du lieu
+//kich thuoc cua kieu khong nguyen thuy la nhu nhau
